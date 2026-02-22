0ee36fc84c647bcbcc836d0aafd7dd5203ccd7a3
Adel bin Mohammed <coe@xavov.com>
2026-02-22 23:01:28 +0300

    Source Control

--- lib/prisma/index.ts
+++ lib/prisma/index.ts
@@ -1,17 +1,13 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client"
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
-
-const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ["query"],
-  });
-
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient | undefined
 }
 
-export default prisma;
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient()
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma
+}
