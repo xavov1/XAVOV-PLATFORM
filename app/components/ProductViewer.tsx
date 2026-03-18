021b4697f6f73cd858d553848d5e64bfc43a04e7
Adel bin Mohammed <coe@xavov.com>
2026-03-18 18:35:27 +0300

    3D test

--- app/components/ProductViewer.tsx
+++ app/components/ProductViewer.tsx
@@ -3,30 +3,19 @@
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls, useGLTF } from '@react-three/drei'
 
-type Props = {
-  modelName: string
+function Model({ modelPath }: { modelPath: string }) {
+  const { scene } = useGLTF(modelPath)
+  return <primitive object={scene} scale={1.5} />
 }
 
-function ProductModel({ modelName }: Props) {
-  const { scene } = useGLTF(`/models/duck/${modelName}.glb`)
-
+export default function ProductViewer() {
   return (
-    <primitive
-      object={scene}
-      scale={3}          // كبرناه
-      position={[0, 0, 0]} // رفعناه
-    />
-  )
-}
-
-export default function ProductViewer({ modelName }: Props) {
-  return (
-    <div style={{ width: '100%', height: '400px' }}>
-      <Canvas camera={{ position: [0, 1.5, 4] }}>
+    <div style={{ width: '100%', height: '500px' }}>
+      <Canvas camera={{ position: [0, 2, 5], fov: 50 }}>
         <ambientLight intensity={1} />
-        <directionalLight position={[2, 2, 2]} />
+        <directionalLight position={[5, 5, 5]} />
 
-        <ProductModel modelName={modelName} />
+        <Model modelPath="/models/duck/fridge.glb" />
 
         <OrbitControls />
       </Canvas>
